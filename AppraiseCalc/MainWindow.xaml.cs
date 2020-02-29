c4af6a79a37ed27df39e166fd2561e0f75d0585f
macmillan333 <tengyuechina@outlook.com>
2020-02-28 21:28:15 -0800

    UI Layout.

--- AppraiseCalc/MainWindow.xaml.cs
+++ AppraiseCalc/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,58 @@ using System.Windows.Shapes;
 
 namespace AppraiseCalc
 {
+    public class Chain
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Chain> results;
         public MainWindow()
         {
+            results = new ObservableCollection<Chain>();
             InitializeComponent();
+            resultGrid.DataContext = results;
+        }
+
+        private void RefreshResults()
+        {
+            int maxWear = 0;
+            if (!int.TryParse(maxWearTextbox.Text, out maxWear))
+            {
+                return;
+            }
+
+            results.Clear();
+            results.Add(new Chain()
+            {
+                Name = "name",
+                Value = maxWear
+            });
+        }
+
+        private void MaxWearTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshResults();
+        }
+
+        private void MaxAttemptTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshResults();
+        }
+
+        private void MaxGpTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshResults();
+        }
+
+        private void ProcChanceTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshResults();
         }
     }
 }
